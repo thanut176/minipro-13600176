02ea855d7b9d4147b61acdb66e11310ba8b7bacc
thanut jingjit <thanutjingjit@thanuts-macbook-pro.local>
2018-11-10 01:11:10 +0700

    Initial Commit

---
+++ Question_13600176/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Question_13600176
+//
+//  Created by thanut jingjit on 10/11/2561 BE.
+//  Copyright © 2561 thanut jingjit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
